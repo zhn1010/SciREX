9cbff2d966ca83629e73ed1035e2a498bf58c257
Saeed <saeed.torabzadeh@gmail.com>
2021-07-22 09:39:27 +0200

    removes extra slash

--- scirex/commands/predict_scirex_model.sh
+++ scirex/commands/predict_scirex_model.sh
@@ -1,5 +1,5 @@
 export test_file=scirex_dataset/release_data/test.jsonl
-export test_output_folder=test_outputs/
+export test_output_folder=test_outputs
 
 echo "Predicting NER"
 python scirex/predictors/predict_ner.py \
