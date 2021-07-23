55c4b7fd5d6597eacc4d5f7b1cb62e286a80d898
Saeed <saeed.torabzadeh@gmail.com>
2021-07-23 12:54:42 +0200

    Print predicted clusters

--- scirex/predictors/utils.py
+++ scirex/predictors/utils.py
@@ -57,8 +57,9 @@ def map_and_intersect_predicted_clusters_to_gold(
     gold_clusters = {k:[tuple(x) for x in v] for k, v in gold_clusters.items()}
 
     predicted_clusters_string = {key: [' '.join(words[span[0]:span[1]]) for span in value] for (key, value) in predicted_clusters.items()}
+    gold_clusters_string = {key: [' '.join(words[span[0]:span[1]]) for span in value] for (key, value) in gold_clusters.items()}
     print('predicted_clusters', predicted_clusters_string)
-    print('gold_clusters', gold_clusters)
+    print('gold_clusters', gold_clusters_string)
     print('words', words)
 
     intersection_scores = intersect_predicted_clusters_to_gold(predicted_clusters, gold_clusters)
